9f7a42d4aa25ad8b45d65aae8710feaeb8257f5b
Daniel Koch <daniel.ukonhattu@gmail.com>
2025-10-17 10:17:20 +0300

    Revert "remove parts that are not used for this version"
    This reverts commit 1b33fef456e1460ad64ca0243a6d31c8f3716aa0.

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -6,6 +6,9 @@ interface Config {
   PORT: number,
   GCAI_URL: string,
   GCAI_TOKEN: string,
+  ACCELBYTE_ADMIN_SECRET: string,
+  ACCELBYTE_ADMIN_CLIENT_ID: string,
+  SERENDIP_WHITELIST_TOKEN: string
 }
 
 const config: Config = {
@@ -14,6 +17,9 @@ const config: Config = {
   PORT: Number(process.env.PORT) || 80,
   GCAI_URL: process.env.GCAI_URL!.trim(),
   GCAI_TOKEN: process.env.GCAI_TOKEN!.trim(),
+  ACCELBYTE_ADMIN_SECRET: process.env.ACCELBYTE_ADMIN_SECRET!.trim(),
+  ACCELBYTE_ADMIN_CLIENT_ID: process.env.ACCELBYTE_ADMIN_CLIENT_ID!.trim(),
+  SERENDIP_WHITELIST_TOKEN: process.env.SERENDIP_WHITELIST_TOKEN!.trim()
 }
 
 export default config;
